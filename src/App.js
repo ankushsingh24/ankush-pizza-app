e33305fbf1284cd6cb6ef4afc44a560a873616ab
Ankush Singh <ankushsahil45@gmail.com>
2022-08-31 23:43:12 +0530

    changing dir

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import "./App.css";
-import Product from "./modules/products/pages/Product";
-// import Gps from "./shared/widgets/Gps";
-
-function App() {
-  return <Product />;
-}
-
-export default App;
