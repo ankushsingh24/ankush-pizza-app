954e9996dd1d0e9b4184f1093d711408abdacfc5
Ankush Singh <ankushsahil45@gmail.com>
2022-08-31 23:56:12 +0530

    added cart

---
+++ backend/routes/cart.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const routes = express.Router();
+
+routes.post("/add-to-cart", (request, response) => {});
+routes.post("/remove-from-cart", (request, response) => {});
+routes.post("/view-cart", (request, response) => {});
+routes.post("/update-cart", (request, response) => {});
